e6e3be29609e1a7f3b5360d4b5ae42b4bc860489
Viktoriya Bortnikova <vikavika233@gmail.com>
2023-02-16 17:18:43 +0400

    fixed menu link

--- src/components/Layout/Navigation/Menu/MenuItem.tsx
+++ src/components/Layout/Navigation/Menu/MenuItem.tsx
@@ -1,3 +1,4 @@
+import { Link } from "react-router-dom";
 import { IMenuItem } from "./menu.types";
 
 interface IMenuProps {
@@ -7,7 +8,7 @@ interface IMenuProps {
 export const MenuItem = ({ item }: IMenuProps) => {
   return (
     <li className="m-5 border-dotted border-black">
-      <a href={item.link}>{item.title} </a>
+      <Link to={item.link}>{item.title}</Link>
     </li>
   );
 };
